be583b793f11a13463195aed4771c06bd9a4b41b
Nazir-Iqbal <naziriqbal01102003@gmail.com>
2025-03-08 12:01:37 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,33 @@
+// const name = "Nazir"
+// const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String("Nazir-FC")
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(1));
+// console.log(gameName.indexOf('z'));
+
+// console.log(gameName)
+// const newString = gameName.substring(0,5)
+// console.log(newString)
+
+// const anotherString = gameName.slice(-7,4)
+// console.log(anotherString)
+
+// const newStringOne = "   Nazir  ";
+// console.log(newStringOne)
+// console.log(newStringOne.trim())
+
+const url = "https://nazir.com//nazir%20iqbal"
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('nazir'))
+
+console.log(gameName.split('-'));
