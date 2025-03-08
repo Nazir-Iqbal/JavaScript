ee6a363c15a88eb4760052806ec70479ae80ff63
Nazir-Iqbal <naziriqbal01102003@gmail.com>
2025-03-08 09:19:33 +0000

    conversion and operations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,73 @@
+let score = "Nazir"
+
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+// isLoggedIn = "Nazir"`
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1=>true ; 0=>false
+// "" => false
+// "Nazir" => true
+// "" => false
+
+// let someNumber = 33
+
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// let objectNumber = Object(someNumber)
+// console.log(objectNumber);
+// console.log(typeof objectNumber)
+
+// **************************** Operations ****************************
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%3)
+
+// let str1 = "Nazir"
+// let str2 = "Iqbal"
+// let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log(typeof (1+"2"))
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log(+true)
+// console.log(+"")
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2+2
+
+// prefix postfix incremental operators
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
