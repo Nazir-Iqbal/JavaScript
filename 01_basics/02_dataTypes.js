ce043aaba79c7a939d327617d07389de876d52fd
Nazir-Iqbal <naziriqbal01102003@gmail.com>
2025-03-08 08:35:17 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, This run in browser
+
+//  data Types
+
+let name = "Nazir" // string
+let age = 18 // number
+let isLoggedIn = false // bool
+
+// number => 2^53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assinged
+// symboll => unique
+
+// object
+
+console.log( typeof "nazir")
+console.log(typeof null) // it is an object
+console.log(typeof undefined) // it is undefined
